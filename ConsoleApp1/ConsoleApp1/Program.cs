ae7c3a51f8b998ef94bbb220c1b22a0d2b09c2a0
yanglongfei <longfei.yang@freemud.cn>
2019-01-24 17:26:04 +0800

    222

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello github 111!");
+            Console.WriteLine("hello github 222!");
         }
     }
 }
